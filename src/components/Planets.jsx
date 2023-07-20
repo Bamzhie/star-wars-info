f4078cd5791cd21c6aacccd1caf31ba1fbb5224c
@bamzhie <sheuahmad@gmail.com>
2023-07-20 20:14:40 +0100

    updated

--- src/components/Planets.jsx
+++ src/components/Planets.jsx
@@ -14,7 +14,7 @@ const Planets = () => {
   });
   return (
     <div className="p-2">
-      <h2 className="mx-4 p-3 p-x-2 text-gray-200 text-xl">People</h2>
+      <h2 className="mx-4 p-3 p-x-2 text-gray-200 text-xl">Planets</h2>
       <div className="w-20 h-1 mx-4 bg-gray-600 mb-7"></div>
       {status === "loading" && <div>Loading data...</div>}
       {status === "error" && <div>Error fetching data</div>}
